10d68e6de3a985ce0c082a7b31622e5ad1ada705
susu7532 <yin7532@gmail.com>
2022-05-28 13:18:43 +0800

    Update AppConfig.java

--- src/main/java/com/foreignCurrency/AppConfig.java
+++ src/main/java/com/foreignCurrency/AppConfig.java
@@ -1,7 +1,6 @@
 package com.foreignCurrency;
 
 import org.springframework.boot.SpringApplication;
-//import org.modelmapper.ModelMapper;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
